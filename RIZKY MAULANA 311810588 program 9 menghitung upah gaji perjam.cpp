81d393e58301616e7d1ce8fb2cfe3cf3b9120aa4
rizkymaulana803 <46336552+rizkymaulana803@users.noreply.github.com>
2019-01-03 18:56:04 +0700

    Add files via upload

---
+++ RIZKY MAULANA 311810588 program 9 menghitung upah gaji perjam.cpp
@@ -0,0 +1,15 @@
+	#include <iostream>
+	using namespace std;
+	
+	int main ()
+	{
+		int gaji,j;
+		cout <<"Masukan Berapa Jam anda Kerja : ";
+		cin >>j;
+		
+		//Rumus
+		gaji = 5000 * j;
+		cout<<"Gaji yang Anda Dapatkan adalah = " <<gaji;
+		
+		return 0;
+	}
